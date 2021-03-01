b1bfd27f93c9d91a06b1980b0dbbd8126ee0531c
bakarerk <er.bakar@gmail.com>
2021-03-01 11:49:03 +0300

    2

---
+++ Bakar_Notlar.py
@@ -0,0 +1,14 @@
+#matplotlib ile grafik çizdirme
+#==============================
+'''
+import matplotlib.pyplot as plt
+
+a = [1,2,3,4,5]
+b =[0.1,0.2,0.3,0.4,0.5]
+
+c = plt.plot(a,b)
+plt.show()
+'''
+#==============================
+
+
